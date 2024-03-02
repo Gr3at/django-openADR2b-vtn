a70be29a3c9c05be724852f669ee37ca457b8308
Gr3at <gr3atdk@gmail.com>
2024-03-03 01:11:53 +0200

    add getting started and test instructions. add test script.

--- scripts/poetry_scripts.py
+++ scripts/poetry_scripts.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 
@@ -10,3 +11,18 @@ def django_makemigrations_and_migrate():
 def django_runserver():
     """Fire up the Django server"""
     os.system("poetry run python src/manage.py runserver")
+
+
+def django_test():
+    """Test Django server"""
+    parser = argparse.ArgumentParser(
+        description="Select specific Django application to test"
+    )
+    parser.add_argument(
+        "--app",
+        dest="selected_apps",
+        default="openADR2b_vtn",
+        help='The Django applications you want to test separated by space (e.g. --app="openADR2b_vtn other_app").',
+    )
+    args = parser.parse_args()
+    os.system(f"poetry run python src/manage.py test {args.selected_apps}")
